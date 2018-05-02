c6fcccac8077769af679b5d3d32e1c19e79b7c35
Jose Fco Rodríguez Hernández <alu0100894646@ull.edu.es>
2018-05-02 11:01:42 +0100

    Pasa la prueba 4

--- tdd.js
+++ tdd.js
@@ -2,12 +2,29 @@ var assert = require('assert');
 
 function Raices(a,b,c){
     
-    var Discriminante = Math.sqrt(b*b-4*a*c);
+    var Discriminante = (b*b-4*a*c);
+    var RaizDiscriminante;
     
-    var R1 = (-b + Discriminante) / (2*a);
-    var R2 = (-b - Discriminante) / (2*a);
+    if(Discriminante > 0)
+    {
+        RaizDiscriminante = Math.sqrt(Discriminante);
     
-    return [[R1,0],[R2,0]];
+        var R1 = (-b + RaizDiscriminante) / (2*a);
+        var R2 = (-b - RaizDiscriminante) / (2*a);
+        var RI1 = 0;
+        var RI2 = 0;
+    }
+    else
+    {
+        RaizDiscriminante = Math.sqrt(-Discriminante);
+        var R1 = -b / (2*a);
+        var R2 = -b / (2*a);
+        var RI1 = RaizDiscriminante / (2*a);
+        var RI2 = -RaizDiscriminante / (2*a);
+        
+    }
+    
+    return [[R1,RI1],[R2,RI2]];
     
 }
 
@@ -15,4 +32,4 @@ function Raices(a,b,c){
 assert.deepEqual(Raices(1,-5,0),[[5,0],[0,0]]);
 assert.deepEqual(Raices(2,5,0),[[0,0],[-2.5,0]]);
 assert.deepEqual(Raices(1,5,0),[[0,0],[-5,0]]);
-assert.deepEqual(Raices(1,0,1),[[0,2],[0-2]]);
+assert.deepEqual(Raices(1,0,1),[[0,1],[0,-1]]);
