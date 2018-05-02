fed19e35745d3c93fdb836d5d63c351ba9fd7434
Jose Fco Rodríguez Hernández <alu0100894646@ull.edu.es>
2018-05-02 10:42:24 +0100

    Pasa la segunda prueba

--- tdd.js
+++ tdd.js
@@ -2,10 +2,13 @@ var assert = require('assert');
 
 function Raices(a,b,c){
     
-    return [[5,0],[2,0]];
+    var R1 = (-b + Math.sqrt(b*b - 4*a*c)) / (2*a);
+    var R2 = (-b - Math.sqrt(b*b - 4*a*c)) / (2*a);
+    
+    return [[R1,0],[R2,0]];
     
 }
 
 
-assert.deepEqual(Raices(1,-5,0),[[5,0],[2,0]]);
-assert.deepEqual(Raices(2,5,0,[[0,0],[2.5,0]]);
+assert.deepEqual(Raices(1,-5,0),[[5,0],[0,0]]);
+assert.deepEqual(Raices(2,5,0),[[0,0],[-2.5,0]]);
